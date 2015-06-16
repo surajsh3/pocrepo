818d0a039fd406279894d23f4d75243cad70af78
SURAJ SHARMA <suraj.aims@gmail.com>
2015-06-16 19:34:42 +0530

    Inital build test

---
+++ src/main/java/TutorialListenerContainer.java
@@ -0,0 +1,12 @@
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TutorialListenerContainer {
+	public static void main(String[] args) {
+		//This will  load the our listener
+		ApplicationContext c1 = new ClassPathXmlApplicationContext("Rabbt-listener-contet.xml");
+		
+	}
+}
